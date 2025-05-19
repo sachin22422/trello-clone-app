0e7df43523ca152d8b49d5c3368010df5698836a
sachin22422 <sachin22422@iiitd.ac.in>
2025-05-19 23:39:10 +0530

    api url add

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://trello-clone-app-3rra.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
